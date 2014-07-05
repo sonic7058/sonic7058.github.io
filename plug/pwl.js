de5c927269e6ab3074696205a1b45f30047d8c04
sonic7058 <butlerl10@gmail.com>
2014-07-05 00:34:47 -0400

    Fourth Of July Changes

--- plug/pwl.js
+++ plug/pwl.js
@@ -25,9 +25,9 @@ pwl.userJoinCallback = function(user) {
         };
         API.on(API.WAIT_LIST_UPDATE, moveDJ);
         API.moderateAddDJ(user.id);
-        API.sendChat('PWL: ' + user.username + ' rejoined within 1 hour of leaving and has been restored to position ' + restoreToPosition + ' in the wait list.');
+        API.sendChat(':us: PWL: ' + user.username + ' rejoined within 1 hour of leaving and has been restored to position ' + restoreToPosition + ' in the wait list.');
       } else {
-        API.sendChat('PWL: ' + user.username + ' rejoined within 1 hour of leaving and should be restored to position ' + restoreToPosition + ' in the wait list.');
+        API.sendChat(':us: PWL: ' + user.username + ' rejoined within 1 hour of leaving and should be restored to position ' + restoreToPosition + ' in the wait list.');
       }
     }
   }
@@ -40,7 +40,7 @@ pwl.chatCallback = function(chatData) {
   if(chatData.message[0] !== ';' && chatData.message[0] !== '!') return;
   if(chatData.message.toLowerCase().substring(1) === 'pwlrunning') {
     if(pwl.engaged) {
-      API.sendChat('PWL is running, @' + chatData.from);
+      API.sendChat(':us: PWL is running, @' + chatData.from);
     }
   }
 }
@@ -48,9 +48,9 @@ pwl.init = function(fullAuto) {
   pwl.stop(true);
   pwl.fullAuto = fullAuto;
   if(fullAuto) {
-    API.sendChat('PWL: Engaged in full auto.');
+    API.sendChat(':us: PWL: Engaged in full auto.');
   } else {
-    API.sendChat('PWL: Engaged.');
+    API.sendChat(':us: PWL: Engaged.');
   }
   API.on(API.USER_JOIN, pwl.userJoinCallback);
   API.on(API.USER_LEAVE, pwl.userLeaveCallback);
@@ -59,7 +59,7 @@ pwl.init = function(fullAuto) {
 }
 pwl.stop = function(stealth) {
   if(!stealth) {
-    API.sendChat('PWL: Disengaged.');
+    API.sendChat(':us: PWL: Disengaged.');
   }
   API.off(API.USER_JOIN, pwl.userJoinCallback);
   API.off(API.USER_LEAVE, pwl.userLeaveCallback);
